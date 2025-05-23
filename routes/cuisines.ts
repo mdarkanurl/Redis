fceb3d436fd10b20bae12687c9b8aba98d8bca2d
mdarkanurl <mdarkanurl@gmail.com>
2025-05-23 14:03:50 -0800

    This get route done /cuisines

--- routes/cuisines.ts
+++ routes/cuisines.ts
@@ -1,4 +1,21 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
+import redis from "../utils/redis";
+import { cuisinesKey } from "../utils/redisKeys";
+import { successResponse } from "../utils/res";
 const router = Router();
+
+router.get('/', async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const cuisines = await redis.smembers(cuisinesKey);
+        successResponse(res, 200, cuisines);
+        return;
+    } catch (error) {
+        next(error);
+    }
+});
 
 export default router;
